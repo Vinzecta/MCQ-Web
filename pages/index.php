9a03d8b7a93ba60b82bbe8fcfb98fffd64902c38
nhanvan2506 <nhanvan2506@gmail.com>
2025-03-17 14:05:58 +0700

    change pages

---
+++ pages/index.php
@@ -0,0 +1,10 @@
+<?php
+    $page = 'home';
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+    }
+    else if (isset($_POST['page'])) {
+        $page = $_POST['page'];
+    }
+    include "$page.php";
+?>
