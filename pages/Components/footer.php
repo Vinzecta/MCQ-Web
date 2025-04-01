ea42fe33ee2097a1c71372fa723c99b4cc0d1044
Lmao1232j <vinhtran23042004@gmail.com>
2025-04-01 09:46:30 +0700

    Change file path for css

--- pages/Components/footer.php
+++ pages/Components/footer.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles/Component/footer.css">
+    <link rel="stylesheet" href="../styles/Component/footer.css">
 </head>
 <body>
     <footer>
