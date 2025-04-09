5aa48fbbe6dde6369034229050e2153c694f3863
Lmao1232j <vinhtran23042004@gmail.com>
2025-04-09 17:42:44 +0700

    Fix js on reset_password page

--- js/Page/reset_password.js
+++ js/Page/reset_password.js
@@ -1,39 +1,84 @@
-import { see_password } from "../Component/see_password.js";
-import { password_requirements } from "../Component/password_requirement.js";
-import { confirm_password } from "../Component/confirm_password.js";
-import { pop_up } from "../Component/pop_up.js";
+import { checksubmit } from "../Component/submit.js";
 
 document.addEventListener("DOMContentLoaded", function() {
-    const checkbox = document.getElementById("show_password");
-    const password_input = document.getElementById("password");
-    const confirm_input = document.getElementById("confirm_password");
-    const navigation = document.getElementById("continue_button");
-    const popup_screen = document.getElementById("pop_up_screen");
+    const alert = document.getElementsByClassName("alert");
+    const email = document.getElementById("email");
+    const show_pass = document.getElementById("check");
+    const confirm = document.getElementById("confirm");
+    const submit = document.getElementById("sign_in_button");
+    const email_regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const password = document.getElementById("password");
+    const special_character_regex = /[^a-zA-Z0-9\s]/;
+    const uppercase_regex = /[A-Z]/;
 
-    checkbox.addEventListener("click", function() {
-        const password = document.getElementById("password");
-        const confirm_password = document.getElementById("confirm_password");
-        see_password(password, confirm_password);
-    });
-
-    password_input.addEventListener("input", function() {
-        password_requirements();
-    });
-
-    confirm_input.addEventListener("input", function() {
-        confirm_password();
-    });
-
-    navigation.addEventListener("click", function(e) {
-        if (password_requirements() && confirm_password()) {
-            e.preventDefault();
-            pop_up(popup_screen);
+    email.addEventListener("input", function() {
+        if (email.value.length == 0) {
+            alert[0].style.display = "block";
         } else {
-            e.preventDefault();
+            alert[0].style.display = "none";
+        }
+        if (!email_regex.test(email.value)) {
+            alert[1].style.display = "block";
+        } else {
+            alert[1].style.display = "none";
         }
     });
+
+    password.addEventListener("input", function() {
+        if (password.value.length  == 0) {
+            alert[2].style.display = "block";
+        } else {
+            alert[2].style.display = "none";
+        }
+
+        if (password.value.length < 8 || password.value.length > 30) {
+            alert[3].style.display = "block";
+        } else {
+            alert[3].style.display = "none";
+        }
+
+        if (!special_character_regex.test(password.value)) {
+            alert[4].style.display = "block";
+        } else {
+            alert[4].style.display = "none";
+        }
+
+        if (!uppercase_regex.test(password.value)) {
+            alert[5].style.display = "block";
+        } else {
+            alert[5].style.display = "none";
+        }
+
+        if (alert[7].style.display == "block") {
+            if (confirm.value == password.value) {
+                alert[7].style.display = "none";
+            }
+        }
+    });
+
+    confirm.addEventListener("input", function() {
+        if (confirm.value.length == 0) {
+            alert[6].style.display = "block";
+        } else {
+            alert[6].style.display = "none";
+        }
+
+        if (confirm.value != password.value) {
+            alert[7].style.display = "block";
+        } else {
+            alert[7].style.display = "none";
+        }
+    });
+
+    show_pass.addEventListener("click", function() {
+        if (show_pass.checked) {
+            password.type = "text";
+        } else {
+            password.type = "password";
+        }
+    });
+
+    submit.addEventListener("click", function(e) {
+        checksubmit(e, alert);
+    });
 });
-
-
-
-
--- pages/reset_password.php
+++ pages/reset_password.php
@@ -5,46 +5,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/Page/reset_password.css">
+    <link href='https://fonts.googleapis.com/css?family=Outfit' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Reset password</title>
  </head>
  <body>
-    <script type="module" src="../js/Page/reset_password.js"></script>
-    <form id="reset_password">
-        <p id="title">Create New Password</p>
-        <p id="instruction">Enter your new password if forgot it!</p>
+    <form class="sign_in">
+        <p id="introduction">Reset Password</p>
+        <!-- Error message for backend (if neccessary)
+        <div class="alert alert-danger" role="alert">New password cannot be the same as old password!</div> -->
 
-        <!--Error message for backend (if neccessary)-->
-        <div class="alert alert-danger" role="alert">New password cannot be the same as old password!</div>
-
-        <div id="form_input">
+        <div class="input_box">
             <input id="email" placeholder="Enter email" type="text">
-            <input id="password" placeholder="Enter password" type="password">
-            <div class="error_message">
-                <div class="alert password_require alert-danger" role="alert">New password must be between 8-30 characters</div>
-                <div class="alert password_require alert-danger" role="alert">New password must contain at least one uppercase character (A-Z)</div>
-                <div class="alert password_require alert-danger" role="alert">New password must contain at least one special character (!@#$)</div>
-            </div>
 
-            <input id="confirm_password" placeholder="Confirm password" type="password">
-            <div class="error_message">
-                <div class="alert password_confirm alert-danger" role="alert">This field is required!</div>
-                <div class="alert password_confirm alert-danger" role="alert">Password do not match!</div>
-            </div>
+            <!--- Email Validation -->
+            <div class="alert alert-danger" role="alert" style="display: none">This field is required!</div>
+            <div class="alert alert-danger" role="alert" style="display: none">Please enter a valid email address!</div>
+
+            <input id="password" placeholder="Enter password" type="password">
+
+            <!--- Password Validation -->
+            <div class="alert alert-danger" role="alert" style="display: none">This field is required!</div>
+            <div class="alert alert-danger" role="alert" style="display: none">Password must be between 8 - 30 characters!</div>
+            <div class="alert alert-danger" role="alert" style="display: none">Password must have at least 1 special character (!@#$)</div>
+            <div class="alert alert-danger" role="alert" style="display: none">Password must have at least 1 uppercase character (A-Z)</div>
+
+            <input id="confirm" placeholder="Confirm password" type="password">
+
+            <!-- Confirm password validation -->
+            <div class="alert alert-danger" role="alert" style="display: none">This field is required!</div>
+            <div class="alert alert-danger" role="alert" style="display: none">Password does not match!</div>
         </div>
-        <input type="checkbox" id="show_password">Show password
+        <div class="check_password">
+            <input id="check" type="checkbox">
+            See password
+        </div>
         <br>
-        <button id="continue_button">Continue</button>
+        <button id="sign_in_button">Continue</button>
     </form>
 
-    <!--Pop up when reset password successfully-->
+    <!-- Pop up when reset password successfully
     <section id="pop_up_screen">
         <div id="popup">
             <img src="../images/reset password/ok_icon.gif" id="ok_icon" alt="OK icon">
             <h1>Reset password successfully!</h1>
             <a href="index.php?page=sign_in"><button id="sign_in_nav">Go to Sign in</button></a>
         </div>
-    </section>
+    </section> -->
+
+    <?php
+        include "./Components/footer.php";
+    ?>
+    <script type="module" src="../js/Page/reset_password.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
  </body>
  </html>
