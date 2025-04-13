716f9bd38d68a6522eb789cd397c58d54af32741
Lmao1232j <vinhtran23042004@gmail.com>
2025-04-13 20:46:25 +0700

    Finish view quiz attempt

--- pages/category.php
+++ pages/category.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/Page/quiz.css">
+    <link rel="stylesheet" href="../styles/Page/category.css">
     <title>Ligma - Quiz</title>
 </head>
 <body>
@@ -12,7 +12,7 @@
         require_once "./Components/search.php";
     ?>
 
-<section class="categories">
+    <section class="categories">
         <div class="category-title">
             <h1>Categories</h1>
             <div id="display">
@@ -25,211 +25,156 @@
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                </div>
+                <p>Math</p>
             </div>
 
             <div class="category">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>
 
             <div class="category">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
+                <p>Math</p>
             </div>  
 
             <div class="category ">
                 <div class="category-image">
                     <img src="../images/explore/math.jpg" alt="math">
                 </div>
-                <div class="quiz-information">
-                        <h3>Math</h3>
-                        <p>Author: abc</p>
-                        <p>Day created: 01/02/2025</p>
-                    </div>
-            </div>  
+                <p>Math</p>
+            </div> 
 
+            <div class="category ">
+                <div class="category-image">
+                    <img src="../images/explore/math.jpg" alt="math">
+                </div>
+                <p>Math</p>
+            </div> 
+
+            <div class="category ">
+                <div class="category-image">
+                    <img src="../images/explore/math.jpg" alt="math">
+                </div>
+                <p>Math</p>
+            </div> 
+
+            <div class="category ">
+                <div class="category-image">
+                    <img src="../images/explore/math.jpg" alt="math">
+                </div>
+                <p>Math</p>
+            </div> 
+            
             <!--Pagination -->
             <div id="pagination">
                 <a><img src="../images/category/left_arrow.png" alt="left-arrow"></a>
@@ -249,7 +194,7 @@
         require_once "./Components/footer.php";
     ?>
 
-    <script src="../js/Page/category.js"></script>
     <script src="../js/Page/explore.js"></script>
+    <script src="../js/Page/category.js"></script>
 </body>
 </html>
